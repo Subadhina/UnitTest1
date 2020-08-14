d9703d4060f55e624265a59887692798f10f51ec
Subadhina <68904668+Subadhina@users.noreply.github.com>
2020-08-14 20:22:42 +0530

    Add files via upload

---
+++ unittest1.java
@@ -0,0 +1,47 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ExamplePrograms;
+/**
+ *
+ * @author ELCOT
+ */
+public class unittest1 {
+    public static void main(String[] args {
+    array s=new array();
+    array.1Darray();
+    array.2Darray();
+   
+    
+    
+}
+
+}   
+class array {
+    int a[]={1,2,3};
+    static void 1Darray()
+    {
+        int a[]={1,2,3};
+      for(int i=0;i<a.length;i++)
+      {
+          System.out.println(a[i]+"\t");
+      }
+     static void 2Darray()
+     {
+         int b[][]={{1,2},{3,4}};
+         for(int i=0;i<b.length;i++)
+         {
+             for(int j=0;j<b[i].length;j++)
+             {
+                 System.out.println(b[i][j]+"\t");
+             }
+         }
+         
+     }
+    }
+    
+          
+      
+    } 
